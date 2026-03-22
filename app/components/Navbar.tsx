ad657d2f97b459eb898c17d0ac1cd2ed5ba3af8c
gabriel cajigas <gabrielcajigas@gabriels-Laptop.local>
2026-03-21 22:21:54 -0400

    chore: auto-lint, type-check, and format

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Navbar() {
           <img
             src="/logo.png"
             alt="Pacheco's Hardwood Floors"
-            style={{ height: "90px", width: "auto", mixBlendMode: "multiply" }}
+            style={{ height: "90px", width: "auto" }}
           />
         </a>
 
